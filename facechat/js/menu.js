c858308bd087bbe9405afe09c706d70588834ebd
soronto3603 <soronto3603@gmail.com>
2017-09-25 19:41:33 +0900

    clean

--- facechat/js/menu.js
+++
@@ -1,4 +0,0 @@
-function click_menu(btn){
-  var url="http://hume.co.kr/facechat/"+btn+".php";
-  window.parent.postMessage('{"title":"move","move":"'+url+'"}',"*");
-}
--- facechat/js/message.js
+++
@@ -1,5 +0,0 @@
-function move_talk_room(target){
-  var phone=document.getElementById('phone').value;
-  var url="http://hume.co.kr/facechat/talk/index.php?phone="+phone+"&target="+target+".php";
-  window.parent.postMessage('{"title":"talk","talk":"'+url+'"}',"*");
-}
